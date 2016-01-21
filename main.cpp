648f175815b16401d90b5fcdf0339b8a9fbfa2ba
Jake Carlson <jrcarlson@smu.edu>
2016-01-20 21:52:03 -0600

    Setup main
    Set up main.cpp using QtCreator and added in the test file provided.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+  Word Search Solver
+  Created by: Jake Carlson
+  Date: 2016-01-20
+
+  This program takes one or more input files of a certain file format and then
+  searches the array of characters at the top of the file for the words stored
+  at the bottom of the file. The progrm then prints information about each word
+  to an output file.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
